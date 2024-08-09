62c8baf3cccb1259789ceeacb5eaf19de6aa5c76
Eric Musyoka <wambua.eric01@gmail.com>
2024-08-09 09:59:57 +0300

    Add endpoint to get map bounds and handle filters and errors

---
+++ routes/bounds.js
@@ -0,0 +1,71 @@
+// route query
+require("dotenv").config()
+
+const sql = (params, query) => {
+    return `
+    SELECT
+      ST_XMin(bbox) AS xMin,
+      ST_YMin(bbox) AS yMin,
+      ST_XMax(bbox) AS xMax,
+      ST_YMax(bbox) AS yMax
+    FROM (
+        SELECT ST_Extent(${process.env.TABLE_COLUMN}) AS bbox
+        FROM ${process.env.TABLE_NAME}
+        WHERE ${`xform_id=${query.form_id} AND geom is not null AND deleted_at is null`}
+    ) AS subquery;
+    `
+  }
+
+  
+ // route schema
+const schema = {
+  description:
+    'Returns forms map bounds',
+  tags: ['feature'],
+  summary: 'Return bounds',
+  querystring: {
+    form_id: {
+      type: 'string | number',
+      description: 'Form is',
+    }
+  }
+}
+  
+// create route
+module.exports = function (fastify, opts, next) {
+  fastify.route({
+    method: 'GET',
+    url: '/bounds',
+    schema: schema,
+    handler: function (request, reply) {
+      fastify.pg.connect(onConnect)
+
+      function onConnect(err, client, release) {
+        if (err) {
+          request.log.error(err)
+          return reply.code(500).send({ error: "Database connection error." })
+        }
+
+        client.query(
+          sql(request.params, request.query),
+          function onResult(err, result) {
+            release()
+            if (err) {
+              reply.code(400).send(err)
+            } else {
+              if(result.rows > 0) {
+                reply.send(result.rows[0])
+              } else {
+                reply.code(404).send({error: 'No data found' });
+              }
+            }
+          }
+        )
+      }
+    }
+  })
+  next()
+}
+  
+  module.exports.autoPrefix = '/v1'
+  
